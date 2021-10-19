45f80a8d826345c549e3701d0dbb6e78cb846d86
GwangIl-Park <rkasud0@gmail.com>
2021-10-19 22:30:02 +0900

    add flume

--- collect/kafka/test_consumer.py
+++ collect/kafka/test_consumer.py
@@ -3,5 +3,4 @@ from kafka import KafkaConsumer
 consumer = KafkaConsumer('test_topic',bootstrap_servers='master:9092,worker1:9092,worker2:9092')
 
 for message in consumer:
-        print("Topic:{},Partition:{}, Offset:{}, Key:{}, Value:{}"
-              .format(message.topic,message.partition,message.offset,message.key,message.value))
+      print(message.value)
