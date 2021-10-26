98df72d0527303422ae08df931a979dc95a87305
GwangIl-Park <40749130+GwangIl-Park@users.noreply.github.com>
2021-10-26 21:33:58 +0900

    Delete test_consumer.py

--- collect/kafka/test_consumer.py
+++
@@ -1,6 +0,0 @@
-from kafka import KafkaConsumer
-
-consumer = KafkaConsumer('test_topic',bootstrap_servers='master:9092,worker1:9092,worker2:9092')
-
-for message in consumer:
-      print(message.value)
